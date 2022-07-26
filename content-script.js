a1afa9ed43e5070011e4e8b6c57ce4ce0149b85c
Huy Nguyen Huu <nguyen.huu.huy@alliedtechbase.com>
2022-07-26 09:27:52 +0700

    Only watch blender clips all time

--- content-script.js
+++ content-script.js
@@ -10,6 +10,13 @@ setInterval(() => {
     let videoTitle = document.querySelector('h1[class *="metadata"]').textContent;
     let chanelName = document.querySelector('#owner-and-teaser [class *="channel"]').textContent;
 
+    if (!videoTitle.match(/blender/i)) {
+        let hour = new Date().getHours();
+        if (hour !== 10) {
+            window.location.href = 'https://www.youtube.com/';
+        }
+    }
+
     chanelBlockeds.forEach((chanel) => {
         if (chanelName.toLowerCase().indexOf(chanel.toLowerCase()) >= 0) {
             window.location.href = 'https://www.youtube.com/';
