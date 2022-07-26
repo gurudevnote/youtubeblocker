2e53d9254d1ff84d7ea4287347103f9c199b96e3
Huy Nguyen Huu <nguyen.huu.huy@alliedtechbase.com>
2022-07-26 09:31:16 +0700

    Refactoring

--- content-script.js
+++ content-script.js
@@ -6,37 +6,41 @@ const videoBlockeds = [
     'diana'
 ];
 
-setInterval(() => {
-    let videoTitle = document.querySelector('h1[class *="metadata"]').textContent;
-    let chanelName = document.querySelector('#owner-and-teaser [class *="channel"]').textContent;
+function domChangedHandle() {
+    return () => {
+        let videoTitle = document.querySelector('h1[class *="metadata"]').textContent;
+        let chanelName = document.querySelector('#owner-and-teaser [class *="channel"]').textContent;
 
-    if (!videoTitle.match(/blender/i)) {
-        let hour = new Date().getHours();
-        if (hour !== 10) {
-            window.location.href = 'https://www.youtube.com/';
+        if (!videoTitle.match(/blender/i)) {
+            let hour = new Date().getHours();
+            if (hour !== 10) {
+                window.location.href = 'https://www.youtube.com/';
+            }
         }
-    }
 
-    chanelBlockeds.forEach((chanel) => {
-        if (chanelName.toLowerCase().indexOf(chanel.toLowerCase()) >= 0) {
-            window.location.href = 'https://www.youtube.com/';
-        }
-    });
-
-    videoBlockeds.forEach((video) => {
-        if (videoTitle.toLowerCase().indexOf(video.toLowerCase()) >= 0) {
-            window.location.href = 'https://www.youtube.com/';
-        }
-    });
-
-    //Youtube search list
-    let videos = document.querySelectorAll('#contents ytd-video-renderer, #contents ytd-channel-renderer');
-    videos.forEach((video) => {
-        let textContext = video.textContent;
-        videoBlockeds.forEach((blockedVideo) => {
-            if (textContext.toLowerCase().indexOf(blockedVideo.toLowerCase()) >= 0) {
-                video.remove();
+        chanelBlockeds.forEach((chanel) => {
+            if (chanelName.toLowerCase().indexOf(chanel.toLowerCase()) >= 0) {
+                window.location.href = 'https://www.youtube.com/';
             }
         });
-    });
-}, 1000);
+
+        videoBlockeds.forEach((video) => {
+            if (videoTitle.toLowerCase().indexOf(video.toLowerCase()) >= 0) {
+                window.location.href = 'https://www.youtube.com/';
+            }
+        });
+
+        //Youtube search list
+        let videos = document.querySelectorAll('#contents ytd-video-renderer, #contents ytd-channel-renderer');
+        videos.forEach((video) => {
+            let textContext = video.textContent;
+            videoBlockeds.forEach((blockedVideo) => {
+                if (textContext.toLowerCase().indexOf(blockedVideo.toLowerCase()) >= 0) {
+                    video.remove();
+                }
+            });
+        });
+    };
+}
+
+setInterval(domChangedHandle(), 1000);
