9bd4525b8e2c3071f51c44394242577d547fa4c2
Huy Nguyen Huu <nguyen.huu.huy@alliedtechbase.com>
2022-07-26 10:06:31 +0700

    Use MutationObserver to handle dom changed

--- content-script.js
+++ content-script.js
@@ -7,40 +7,48 @@ const videoBlockeds = [
 ];
 
 function domChangedHandle() {
-    return () => {
-        let videoTitle = document.querySelector('h1[class *="metadata"]').textContent;
-        let chanelName = document.querySelector('#owner-and-teaser [class *="channel"]').textContent;
+    let video = document.querySelector('h1[class *="metadata"]');
+    let videoTitle = video ? video.textContent : '';
+    let chanel = document.querySelector('#owner-and-teaser [class *="channel"]');
+    let chanelName = chanel ? chanel.textContent : '';
 
-        if (!videoTitle.match(/blender/i)) {
-            let hour = new Date().getHours();
-            if (hour !== 10) {
-                window.location.href = 'https://www.youtube.com/';
-            }
+    if (
+        videoTitle !== ''
+        && !videoTitle.match(/blender/i)
+        && window.location.href !== 'https://www.youtube.com/'
+    ) {
+        let hour = new Date().getHours();
+        if (hour !== 10) {
+            window.location.href = 'https://www.youtube.com/';
         }
+    }
 
-        chanelBlockeds.forEach((chanel) => {
-            if (chanelName.toLowerCase().indexOf(chanel.toLowerCase()) >= 0) {
-                window.location.href = 'https://www.youtube.com/';
+    chanelBlockeds.forEach((chanel) => {
+        if (chanelName.toLowerCase().indexOf(chanel.toLowerCase()) >= 0) {
+            window.location.href = 'https://www.youtube.com/';
+        }
+    });
+
+    videoBlockeds.forEach((video) => {
+        if (videoTitle.toLowerCase().indexOf(video.toLowerCase()) >= 0) {
+            window.location.href = 'https://www.youtube.com/';
+        }
+    });
+
+    //Youtube search list
+    let videos = document.querySelectorAll('#contents ytd-video-renderer, #contents ytd-channel-renderer');
+    videos.forEach((video) => {
+        let textContext = video.textContent;
+        videoBlockeds.forEach((blockedVideo) => {
+            if (textContext.toLowerCase().indexOf(blockedVideo.toLowerCase()) >= 0) {
+                video.remove();
             }
         });
-
-        videoBlockeds.forEach((video) => {
-            if (videoTitle.toLowerCase().indexOf(video.toLowerCase()) >= 0) {
-                window.location.href = 'https://www.youtube.com/';
-            }
-        });
-
-        //Youtube search list
-        let videos = document.querySelectorAll('#contents ytd-video-renderer, #contents ytd-channel-renderer');
-        videos.forEach((video) => {
-            let textContext = video.textContent;
-            videoBlockeds.forEach((blockedVideo) => {
-                if (textContext.toLowerCase().indexOf(blockedVideo.toLowerCase()) >= 0) {
-                    video.remove();
-                }
-            });
-        });
-    };
+    });
 }
 
-setInterval(domChangedHandle(), 1000);
+
+const observer = new MutationObserver(domChangedHandle)
+const targetNode = document.body;
+const config = { attributes: true, childList: true, subtree: true };
+observer.observe(targetNode, config);
